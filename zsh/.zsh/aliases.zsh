77760ff59d8aae4f0329cfb8c425d2fbb076ebd1
Ryan Mossor <ryan.mossor@gmail.com>
2023-11-30 05:42:58 -0600

    clip aliasclip alias

--- zsh/.zsh/aliases.zsh
+++ zsh/.zsh/aliases.zsh
@@ -2,6 +2,7 @@
 alias history='history -f 1'
 alias x=extract
 alias xsc='xclip -selection clipboard'
+alias clip='xclip -selection clipboard'
 
 alias cgc='cat ~/.gitconfig'
 alias gp='git push origin HEAD'
@@ -16,7 +17,7 @@ alias evrc='$EDITOR ~/.vimrc'
 alias hosts='cat ~/.ssh/config'
 
 fe() { du -a --exclude=".git" --exclude="node_modules" | awk '{print $2}' | fzf | xargs -ro $EDITOR ;}
-fx() { du -a --exclude=".git" --exclude="node_modules" | awk '{print $2}' | fzf | xargs realpath | tr '\n' ' ' | xsc ;}
+fx() { du -a --exclude=".git" --exclude="node_modules" | awk '{print $2}' | fzf | xargs realpath | tr '\n' ' ' | xclip -selection clipboard ;}
 
 clear_git_branches() {
     git branch | grep -vE "main|master|DEV" | xargs git branch -D
