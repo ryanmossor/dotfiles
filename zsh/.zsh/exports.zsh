27c582cdd155fc38445494e888e5a4b63161596f
Ryan Mossor <ryan.mossor@gmail.com>
2024-12-03 15:59:39 -0600

    changes dotfiles location

--- zsh/.zsh/exports.zsh
+++ zsh/.zsh/exports.zsh
@@ -3,7 +3,7 @@ export MANWIDTH=999
 export PATH="$HOME/.local/bin":$PATH
 export DOTNET_CLI_TELEMETRY_OPTOUT=1
 
-[[ -z $DOTFILES ]] && export DOTFILES="$HOME/dotfiles"
+[[ -z $DOTFILES ]] && export DOTFILES="$HOME/code/dotfiles"
 [[ -x /usr/bin/batcat ]] && export BAT="/usr/bin/batcat"
 [[ -x /usr/local/bin/bat ]] && export BAT="/usr/local/bin/bat"
 [[ -x /usr/local/go/bin ]] && export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin
