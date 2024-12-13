0d43e926513a904584c6b7615bcef69f5a3d8a67
Ryan Mossor <ryan.mossor@gmail.com>
2024-12-12 22:46:49 -0600

    removes unnecessary FD var

--- config/zsh/.zsh/exports.zsh
+++ config/zsh/.zsh/exports.zsh
@@ -9,13 +9,7 @@ export DOTNET_CLI_TELEMETRY_OPTOUT=1
 [[ -x /usr/local/bin/bat ]] && export BAT="/usr/local/bin/bat"
 [[ -x /usr/local/go/bin ]] && export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin
 
-if command -v fdfind &> /dev/null; then
-    export FD='fdfind'
-elif command -v fd &> /dev/null; then
-    export FD='fd'
-fi
-
-export FZF_DEFAULT_COMMAND="$FD --hidden --follow --exclude .git --exclude node_modules"
+export FZF_DEFAULT_COMMAND="fd --hidden --follow --exclude .git --exclude node_modules"
 export FZF_DEFAULT_OPTS="--highlight-line --reverse --border=rounded --info=inline-right --marker ▏ --pointer ▌ \
     --color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
     --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
