31daccd77823a2de63d7643820c74d6a718c5612
Ryan Mossor <ryan.mossor@gmail.com>
2024-12-16 17:48:16 -0600

    lualine

--- config/nvim/.config/nvim/lua/plugins/lualine.lua
+++ config/nvim/.config/nvim/lua/plugins/lualine.lua
@@ -1,14 +1,22 @@
 return {
-    "nvim-lualine/lualine.nvim",
-    config = function()
-        require("lualine").setup({
-            options = {
-              section_separators = "",
-              component_separators = "|",
-            },
-            sections = {
-                lualine_x = { "encoding", "filetype" },
-            },
-        })
-    end,
+	"nvim-lualine/lualine.nvim",
+	config = function()
+		require("lualine").setup({
+			options = {
+				section_separators = "",
+				component_separators = "|",
+			},
+			sections = {
+				lualine_b = {
+					{ "branch", icon = "󰘬" },
+					"diff",
+					"diagnostics",
+				},
+				lualine_x = {
+					"encoding",
+					"filetype",
+				},
+			},
+		})
+	end,
 }
