6d2d5e2024042b8fdedbe8804156d1995b0ce0e8
Ryan Mossor <ryan.mossor@gmail.com>
2024-12-19 23:39:57 -0600

    spotify web app

--- setup/linux-desktop/spotify.sh
+++ setup/linux-desktop/spotify.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 # shellcheck disable=2154
 
+# TODO: remove -- replaced with brave web app
+exit 0
+
 if ! have spotify; then
     curl -sS https://download.spotify.com/debian/pubkey_6224F9941A8AA6D1.gpg | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
     echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
