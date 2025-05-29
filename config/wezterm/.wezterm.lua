18bfe419283d735b2be2ab0a1b88c6367c2b4738
Ryan Mossor <ryan.mossor@gmail.com>
2025-05-29 11:17:44 -0500

    wezterm opacity

--- config/wezterm/.wezterm.lua
+++ config/wezterm/.wezterm.lua
@@ -11,32 +11,32 @@ config.term = "xterm-256color"
 
 local catppuccin_mocha = wezterm.get_builtin_color_schemes()["Catppuccin Mocha"]
 catppuccin_mocha.tab_bar = {
-	background = catppuccin_mocha.tab_bar.background,
+    background = catppuccin_mocha.tab_bar.background,
 
-	active_tab = {
-		bg_color = "#89b4fa",
-		fg_color = "#11111b",
-		intensity = "Normal", -- "Half", "Normal", or "Bold"
-	},
+    active_tab = {
+        bg_color = "#89b4fa",
+        fg_color = "#11111b",
+        intensity = "Normal", -- "Half", "Normal", or "Bold"
+    },
 
-	inactive_tab = {
-		bg_color = catppuccin_mocha.tab_bar.background,
-		fg_color = "#7f849c",
-		intensity = "Normal",
-	},
+    inactive_tab = {
+        bg_color = catppuccin_mocha.tab_bar.background,
+        fg_color = "#7f849c",
+        intensity = "Normal",
+    },
 
-	inactive_tab_hover = catppuccin_mocha.tab_bar.inactive_tab_hover,
+    inactive_tab_hover = catppuccin_mocha.tab_bar.inactive_tab_hover,
 
-	new_tab = {
-		bg_color = "#1b1032",
-		fg_color = "#808080",
-	},
+    new_tab = {
+        bg_color = "#1b1032",
+        fg_color = "#808080",
+    },
 
-	new_tab_hover = catppuccin_mocha.tab_bar.new_tab_hover,
+    new_tab_hover = catppuccin_mocha.tab_bar.new_tab_hover,
 }
 
 config.color_schemes = {
-	["Catppuccin Mocha"] = catppuccin_mocha,
+    ["Catppuccin Mocha"] = catppuccin_mocha,
 }
 
 config.color_scheme = "Catppuccin Mocha"
@@ -52,17 +52,17 @@ local mod = is_mac and "CMD|SHIFT" or "CTRL|SHIFT"
 local ctrl = is_mac and "CMD" or "CTRL"
 
 config.keys = {
-	{ key = "w", mods = mod, action = act.CloseCurrentTab({ confirm = true }) },
-	{ key = "v", mods = mod, action = act.PasteFrom("Clipboard") },
-	-- { key = 'v', mods = mod, action = act.PasteFrom 'PrimarySelection' },
-	{ key = "c", mods = mod, action = act.ActivateCopyMode },
-	{ key = "e", mods = mod, action = wezterm.action_callback(vim_edit_scrollback) },
-	{ key = "t", mods = mod, action = act.SpawnTab "CurrentPaneDomain" },
+    { key = "w", mods = mod, action = act.CloseCurrentTab({ confirm = true }) },
+    { key = "v", mods = mod, action = act.PasteFrom("Clipboard") },
+    -- { key = 'v', mods = mod, action = act.PasteFrom 'PrimarySelection' },
+    { key = "c", mods = mod, action = act.ActivateCopyMode },
+    { key = "e", mods = mod, action = wezterm.action_callback(vim_edit_scrollback) },
+    { key = "t", mods = mod, action = act.SpawnTab("CurrentPaneDomain") },
 }
 
 -- Ctrl+1 for tab 1, Ctrl+2 for tab 2, etc.
 for i = 1, 9 do
-	table.insert(config.keys, { key = tostring(i), mods = "CTRL", action = act.ActivateTab(i - 1) })
+    table.insert(config.keys, { key = tostring(i), mods = "CTRL", action = act.ActivateTab(i - 1) })
 end
 
 config.mouse_bindings = {
@@ -86,33 +86,8 @@ config.tab_max_width = 32
 
 -- font
 config.font = wezterm.font_with_fallback({ "JetBrains Mono NL Medium", "Cascadia Mono", "Ubuntu Mono" })
-config.harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' } -- disable font ligatures
+config.harfbuzz_features = { "calt=0", "clig=0", "liga=0" } -- disable font ligatures
 config.font_size = 16
-
-local default_opacity = 0.90
-if is_windows then
-	config.default_prog = { "wsl.exe", "~" }
-	default_opacity = 1.0
-
-	table.insert(config.keys, {
-		key = "p",
-		mods = "ALT",
-		action = act.SpawnCommandInNewTab({ args = { "powershell.exe", "-NoLogo" } }),
-	})
-else
-	config.window_background_opacity = default_opacity
-	table.insert(config.keys, { key = "o", mods = mod, action = wezterm.action.EmitEvent("toggle-opacity") })
-end
-
-wezterm.on("toggle-opacity", function(window)
-	local overrides = window:get_config_overrides() or {}
-	if overrides.window_background_opacity == 1.0 then
-		overrides.window_background_opacity = default_opacity
-	else
-		overrides.window_background_opacity = 1.0
-	end
-	window:set_config_overrides(overrides)
-end)
 
 config.animation_fps = 1
 config.cursor_blink_ease_in = "Constant"
@@ -121,36 +96,36 @@ config.cursor_blink_ease_out = "Constant"
 config.max_fps = 120
 
 function tab_title(tab_info)
-	local title = tab_info.tab_title
-	-- if the tab title is explicitly set, take that
-	if title and #title > 0 then
-		return title
-	end
-	-- Otherwise, use the title from the active pane in that tab
-	return tab_info.active_pane.title
+    local title = tab_info.tab_title
+    -- if the tab title is explicitly set, take that
+    if title and #title > 0 then
+        return title
+    end
+    -- Otherwise, use the title from the active pane in that tab
+    return tab_info.active_pane.title
 end
 
 wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
-	local format_str = "%s:%s"
+    local format_str = "%s:%s"
 
-	local has_unseen_output = false
-	for _, pane in ipairs(tab.panes) do
-		if pane.has_unseen_output then
-			has_unseen_output = true
-			break
-		end
-	end
-	if has_unseen_output then
+    local has_unseen_output = false
+    for _, pane in ipairs(tab.panes) do
+        if pane.has_unseen_output then
+            has_unseen_output = true
+            break
+        end
+    end
+    if has_unseen_output then
         format_str = format_str .. "*"
-	end
+    end
 
-	if tab.is_active then
+    if tab.is_active then
         format_str = "[" .. format_str .. "]"
-	end
+    end
 
-	return {
-		{ Text = " " .. string.format(format_str, tab.tab_index + 1, tab_title(tab)) .. " " },
-	}
+    return {
+        { Text = " " .. string.format(format_str, tab.tab_index + 1, tab_title(tab)) .. " " },
+    }
 end)
 
 return config
