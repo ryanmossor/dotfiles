6e1a746417b71ff97a75118192aaedae2efe8027
Ryan Mossor <ryanmossor@M4-Max.local>
2025-06-18 17:49:43 -0500

    update mac work alias

--- config/zsh/.zsh/aliases/macos-aliases.zsh
+++ config/zsh/.zsh/aliases/macos-aliases.zsh
@@ -1,1 +1,24 @@
-alias work='open /Applications/Slack.app && open /Applications/Microsoft\ Teams.app && open /Applications/Microsoft\ Outlook.app'
+function work() {
+  switch_desktop() {
+    case $1 in
+      1) key=18 ;; # 1
+      2) key=19 ;; # 2
+      3) key=20 ;; # 3
+      4) key=21 ;; # 4
+      5) key=23 ;; # 5
+      *) echo "Usage: switch_desktop <1-5>"; return 1 ;;
+    esac
+    osascript -e "tell application \"System Events\" to key code $key using control down"
+    sleep 1
+  }
+
+  switch_desktop 3
+  open -a "Microsoft Teams"
+
+  switch_desktop 4
+  open -a "Microsoft Outlook"
+
+  switch_desktop 5
+  open -a "Slack"
+}
+
