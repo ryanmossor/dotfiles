2700586bf9735ebc3860e71e5d0c6b5536031b62
Ryan Mossor <ryan.mossor@gmail.com>
2026-02-12 20:24:46 -0600

    dotnet

---
+++ setup/linux-desktop/dotnet.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+if ! have dotnet; then
+    sudo add-apt-repository ppa:dotnet/backports
+    sudo apt-get update
+    sudo apt-get install dotnet-sdk-9.0 dotnet-sdk-8.0 -y
+fi
+
