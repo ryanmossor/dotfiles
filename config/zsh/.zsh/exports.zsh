e18011d5e7c9790474872e967b1d5c80742da41f
Ryan Mossor <ryan.mossor@gmail.com>
2026-03-06 18:44:53 -0600

    ghostty ssh TERM hack

--- config/zsh/.zsh/exports.zsh
+++ config/zsh/.zsh/exports.zsh
@@ -35,6 +35,6 @@ export XDG_CONFIG_HOME=$HOME/.config
 export XDG_CACHE_HOME=$HOME/.cache
 export XDG_DATA_HOME=$HOME/.local/share
 
-if [ "$TERM_PROGRAM" = "ghostty" ]; then
+if [ -n "$SSH_CONNECTION" ]; then
     export TERM=xterm-256color
 fi
