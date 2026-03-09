a3f93907ef39ef468e9634676a6724976a860a0b
Ryan Mossor <ryan.mossor@gmail.com>
2026-03-09 17:17:03 -0500

    dotnet 10

--- setup/linux-desktop/dotnet.sh
+++ setup/linux-desktop/dotnet.sh
@@ -3,6 +3,6 @@
 if ! have dotnet; then
     sudo add-apt-repository ppa:dotnet/backports
     sudo apt-get update
-    sudo apt-get install dotnet-sdk-9.0 dotnet-sdk-8.0 -y
+    sudo apt-get install dotnet-sdk-10.0 dotnet-sdk-9.0 dotnet-sdk-8.0 -y
 fi
 
