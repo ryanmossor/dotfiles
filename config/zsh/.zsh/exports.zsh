f5f0f1ed1b5b59564ba47ac69a8a8a364f896a6e
Ryan Mossor <ryan.mossor@gmail.com>
2026-04-30 20:06:26 -0500

    tmux/fzf tweaks

--- config/zsh/.zsh/exports.zsh
+++ config/zsh/.zsh/exports.zsh
@@ -31,7 +31,7 @@ export FZF_DEFAULT_OPTS="--highlight-line \
     --marker ▏ \
     --pointer ▌ \
     --color=bg+:#313244,spinner:#F5E0DC,hl:#F38BA8 \
-    --color=fg:#CDD6F4,header:#F38BA8,info:#CBA6F7,pointer:#F5E0DC \
+    --color=fg:#CDD6F4,header:#CBA6F7,info:#CBA6F7,pointer:#F5E0DC \
     --color=marker:#B4BEFE,fg+:#CDD6F4,prompt:#CBA6F7,hl+:#F38BA8 \
     --color=selected-bg:#45475A \
     --color=border:#6C7086,label:#CDD6F4"
