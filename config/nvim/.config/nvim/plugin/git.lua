8e69b72a2d1bdaad3e89706501ffd538d4a3eb34
Ryan Mossor <ryan.mossor@gmail.com>
2026-05-11 18:16:02 -0500

    neovim git keymaps

--- config/nvim/.config/nvim/plugin/git.lua
+++ config/nvim/.config/nvim/plugin/git.lua
@@ -1,4 +1,11 @@
 vim.pack.add({ "https://github.com/lewis6991/gitsigns.nvim" })
-require("gitsigns").setup()
 
-vim.keymap.set("n", "<leader>gd", ":Gitsigns preview_hunk<CR>", { desc = "Preview git diff", silent = true })
+require("gitsigns").setup({
+	on_attach = function()
+		local gitsigns = require("gitsigns")
+
+		vim.keymap.set("n", "<leader>gd", gitsigns.preview_hunk_inline, { desc = "Preview git diff", silent = true })
+		vim.keymap.set("n", "]g", gitsigns.next_hunk, { desc = "Next git hunk", silent = true })
+		vim.keymap.set("n", "[g", gitsigns.prev_hunk, { desc = "Previous git hunk", silent = true })
+	end,
+})
