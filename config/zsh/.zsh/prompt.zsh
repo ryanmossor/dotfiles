13556f5cca1261ae33aafe70770b1f4213aa15e7
Ryan Mossor <ryan.mossor@gmail.com>
2026-05-15 08:02:08 -0500

    zsh prompt

--- config/zsh/.zsh/prompt.zsh
+++ config/zsh/.zsh/prompt.zsh
@@ -1,16 +1,15 @@
 parse_git_branch() {
-    git branch 2> /dev/null | sed -e "/^[^*]/d" -e "s/* \(.*\)/ 󰘬 \1/"
+    git branch 2> /dev/null | sed -e "/^[^*]/d" -e "s/* \(.*\)/󰘬 \1/"
 }
 
-autoload -Uz colors && colors
 setopt PROMPT_SUBST
 
-ARROW='%(?.%{$fg_bold[green]%}→.%{$fg_bold[red]%}✘)%{$reset_color%}'
-DIR="%{$fg_bold[blue]%}%~%{$reset_color%}"
-BRANCH="%{$fg_bold[yellow]%}\$(parse_git_branch)%{$reset_color%}"
+ARROW='%(?.%F{green}.%F{red})→%f'
+DIR='%F{blue}%~%f'
+BRANCH='%F{yellow}$(parse_git_branch)%f'
 
 if [[ -n "$SSH_CLIENT" ]] || [[ -n "$SSH_TTY" ]]; then
-    HOST_NAME="%{$fg_bold[green]%}%n@%m%{$reset_color%} "
+    HOST_NAME='%F{green}%n@%m%f '
 fi
 
-PS1="%B${ARROW} ${HOST_NAME}${DIR}${BRANCH}%b "
+PS1="%B${ARROW} ${HOST_NAME}${DIR} ${BRANCH}%b "
